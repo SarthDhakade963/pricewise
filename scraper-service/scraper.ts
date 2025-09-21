b957d65c1aee0079c8474e38a1b2cbde0f0b30bd
Sarthak <sarthakdhakde@gmail.com>
2025-09-21 19:09:32 +0530

    update chromium path

--- scraper-service/scraper.ts
+++ scraper-service/scraper.ts
@@ -15,7 +15,7 @@ export async function scrapeAmazonProduct(productURL: string) {
   // Launch browser
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: "/usr/bin/chromium",
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
