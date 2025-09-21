0f2025fae6dc01055fe9dd76eef6f554615d956e
Sarthak <sarthakdhakde@gmail.com>
2025-09-21 14:49:10 +0530

    fix puppeteer chrome executable path for render

--- scraper-service/scraper.ts
+++ scraper-service/scraper.ts
@@ -15,7 +15,8 @@ export async function scrapeAmazonProduct(productURL: string) {
   // Launch browser
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
